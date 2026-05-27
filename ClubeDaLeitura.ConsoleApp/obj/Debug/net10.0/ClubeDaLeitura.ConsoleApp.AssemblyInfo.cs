87aeb47865697d3ba8138428ff6c5800601b7907
Felipe Neto Mello <fnm03br@gmail.com>
2026-05-26 21:34:56 -0300

    Ajustada tabela de visualização

--- ClubeDaLeitura.ConsoleApp/obj/Debug/net10.0/ClubeDaLeitura.ConsoleApp.AssemblyInfo.cs
+++ ClubeDaLeitura.ConsoleApp/obj/Debug/net10.0/ClubeDaLeitura.ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClubeDaLeitura.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13c8f5f04908a5297711299d2c3316c2a5e05a3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94408e2049e2234d65ca9757db6c74fa9ab6e2b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClubeDaLeitura.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClubeDaLeitura.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
