cbd07be6f243075fbf165e5a73908e1d08354652
Felipe Neto Mello <fnm03br@gmail.com>
2026-05-26 21:24:19 -0300

    Adicionado loop menu caixa

--- ClubeDaLeitura.ConsoleApp/Program.cs
+++ ClubeDaLeitura.ConsoleApp/Program.cs
@@ -15,36 +15,40 @@ while (true)
     if (opcaoMenuPrincipal == "S")
         break;
 
-    if (opcaoMenuPrincipal == "1") //caixas
+    while (true)
     {
-        string? opcaoMenuInterno = telaCaixa.ObterOpcaoMenu();
+        if (opcaoMenuPrincipal == "1") //caixas
+        {
+            string? opcaoMenuInterno = telaCaixa.ObterOpcaoMenu();
 
-        if (opcaoMenuPrincipal == "S")
-            break;
+            if (opcaoMenuInterno == "S")
+                break;
 
-        if (opcaoMenuInterno == "1")
-            telaCaixa.Cadastrar();
+            if (opcaoMenuInterno == "1")
+                telaCaixa.Cadastrar();
 
-        else if (opcaoMenuInterno == "2")
-            telaCaixa.Editar();
+            else if (opcaoMenuInterno == "2")
+                telaCaixa.Editar();
 
-        else if (opcaoMenuInterno == "3")
-            telaCaixa.Excluir();
+            else if (opcaoMenuInterno == "3")
+                telaCaixa.Excluir();
 
-        else if (opcaoMenuInterno == "4")
-            telaCaixa.VisualizarTodos(true);
+            else if (opcaoMenuInterno == "4")
+                telaCaixa.VisualizarTodos(true);
 
-    }
-    else if (opcaoMenuPrincipal == "2") //revistas
-    {
+        }
 
-    }
-    else if (opcaoMenuPrincipal == "3") //amigos
-    {
+        else if (opcaoMenuPrincipal == "2") //revistas
+        {
 
-    }
-    else if (opcaoMenuPrincipal == "4") //empréstimos
-    {
+        }
+        else if (opcaoMenuPrincipal == "3") //amigos
+        {
 
+        }
+        else if (opcaoMenuPrincipal == "4") //empréstimos
+        {
+
+        }
     }
 }
--- ClubeDaLeitura.ConsoleApp/obj/Debug/net10.0/ClubeDaLeitura.ConsoleApp.AssemblyInfo.cs
+++ ClubeDaLeitura.ConsoleApp/obj/Debug/net10.0/ClubeDaLeitura.ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClubeDaLeitura.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdcf417f4623637230e91d094a7a7caabfb5fc1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13c8f5f04908a5297711299d2c3316c2a5e05a3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClubeDaLeitura.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClubeDaLeitura.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
