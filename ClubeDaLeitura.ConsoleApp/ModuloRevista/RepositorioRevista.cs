59b84b9a08138adae6accc85877eb4b82f406c0a
Felipe Neto Mello <fnm03br@gmail.com>
2026-05-29 05:30:18 -0300

    Adicionada Edição de Revistas

--- ClubeDaLeitura.ConsoleApp/ModuloRevista/RepositorioRevista.cs
+++ ClubeDaLeitura.ConsoleApp/ModuloRevista/RepositorioRevista.cs
@@ -20,4 +20,29 @@ public class RepositorioRevista
     {
         return registros;
     }
+
+    public bool Editar(int idSelecionado, Revista revistaAtualizada)
+    {
+        Revista? revistaSelecionada = null;
+
+        for (int i = 0; i < registros.Length; i++)
+        {
+            Revista r = registros[i];
+
+            if (r == null)
+                continue;
+
+            if (r.Id == idSelecionado)
+            {
+                revistaSelecionada = r;
+                break;
+            }
+        }
+        if (revistaSelecionada == null)
+            return false;
+
+        revistaSelecionada.Atualizar(revistaAtualizada);
+
+        return true;
+    }
 }
--- ClubeDaLeitura.ConsoleApp/ModuloRevista/TelaRevista.cs
+++ ClubeDaLeitura.ConsoleApp/ModuloRevista/TelaRevista.cs
@@ -48,7 +48,27 @@ public class TelaRevista
 
     public void Editar()
     {
-        throw new NotImplementedException();
+        Console.WriteLine("------------------------");
+        Console.WriteLine("Edição de Revistas");
+        Console.WriteLine("------------------------");
+
+        VisualizarTodos(false); //false para não mostrar o cabeçalho
+
+        Console.WriteLine("------------------------");
+
+        Console.Write("Digite o ID do registro que deseja editar: ");
+        int idSelecionado = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("------------------------");
+
+        Revista revistaAtualizada = ObterDadosCadastrais();
+
+        repositorioRevista.Editar(idSelecionado, revistaAtualizada);
+
+        Console.WriteLine($"O registro \"{revistaAtualizada.Titulo}\" foi editado com sucesso.");
+        Console.WriteLine("------------------------");
+        Console.WriteLine("Digite ENTER para continuar...");
+        Console.ReadLine();
     }
 
     public void Excluir()
